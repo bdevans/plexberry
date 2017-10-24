d64181d0b67b6b48ba01d4a5cbc86517845f2354
Ben Evans <ben.d.evans@gmail.com>
2017-10-24 13:24:12 +0100

    Tidy up

--- setup_plex.sh
+++ setup_plex.sh
@@ -8,43 +8,19 @@ DISK=${1:-sda1}
 FORMAT=${2:-ext4} #exfat #vfat ntfs
 
 
-
+# Update packages and distribution
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get update && sudo apt-get dist-upgrade
 
-sudo apt-get install libexpat1 -y
+# Install dependencies, key and Plex package
 sudo apt-get update && sudo apt-get install apt-transport-https binutils -y --force-yes
 wget -O - https://dev2day.de/pms/dev2day-pms.gpg.key | sudo apt-key add -
 echo "deb https://dev2day.de/pms/ $VERSION main" | sudo tee /etc/apt/sources.list.d/pms.list
-sudo apt-get update
+sudo apt-get update  # Necessary after adding the new repository
 sudo apt-get install -t $VERSION plexmediaserver -y
-sudo apt-get update && sudo apt-get upgrade -y
-
-# Create autostart script
-#cp plexmediaserver /usr/local/bin/
-#chmod +x /usr/local/bin/plexmediaserver
-
-# Create autorestart script
-#cp schedule_restart /etc/init.d/plexmediaserver
-#chmod +x /etc/init.d/plexmediaserver
-#update-rc.d plexmediaserver defaults
+#sudo apt-get update && sudo apt-get upgrade -y
 
 # Install transcoders
-#sudo service plexmediaserver stop
-#PATH=/usr/lib/plexmediaserver
-#PATH=/opt/plex/Application
-#cd /tmp/
-#mkdir libc6
-#cd libc6
-#wget http://ftp.us.debian.org/debian/pool/main/g/glibc/libc-bin_2.19-18+deb8u2_armhf.deb
-#dpkg-deb -x libc6_2.19-18_armhf.deb ./
-#cp -a lib/arm-linux-gnueabihf/libm-2.19.so $PATH
-#cd $PATH
-#chmod ugo+x libm-2.19.so
-#unlink libm.so.6
-#ln -s libm-2.19.so libm.so.6
-#sudo service plexmediaserver start
-
 sudo apt-get install mkvtoolnix libexpat1 ffmpeg -y
 sudo service plexmediaserver restart
 
@@ -52,6 +28,7 @@ sudo service plexmediaserver restart
 sudo usermod -aG pi plex
 sudo usermod -aG plex pi
 
+echo "Plex server installation complete!"
 
 echo "This will mount disk $DISK with the filesystem $FORMAT"
 DEFAULT="Y"
@@ -86,13 +63,13 @@ else
     echo "Skipping disk mounting..."
 fi
 
-echo "Plex server installation complete!"
 
+# Upgrade firmware and reboot
 read -p "Update firmware and reboot now? [Y/n] " -n 1 -r REPLY
 if [[ $REPLY =~ ^[Yy]$ ]];
 then
     sudo apt-get update && sudo rpi-update
     sudo apt-get autoremove && sudo apt-get clean && sudo reboot
 else
-    echo "Please reboot manually. Exiting!"
+    echo "Please reboot manually before configuring Plex in a browser. Exiting!"
 fi
