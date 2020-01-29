8fb7bc123fe28343fd9e3e1783d15915a9003dbf
Ben Evans <ben.d.evans@gmail.com>
2020-01-29 16:13:57 +0000

    Use new official installer

--- setup_plex.sh
+++ setup_plex.sh
@@ -17,10 +17,10 @@ sudo apt-get update && sudo apt-get dist-upgrade
 
 sudo apt-get install libexpat1 -y
 sudo apt-get update && sudo apt-get install apt-transport-https binutils -y --force-yes
-wget -O - https://dev2day.de/pms/dev2day-pms.gpg.key | sudo apt-key add -
-echo "deb https://dev2day.de/pms/ $VERSION main" | sudo tee /etc/apt/sources.list.d/pms.list
+wget -O - https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
+echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 sudo apt-get update
-sudo apt-get install -t $VERSION plexmediaserver -y
+sudo apt-get install plexmediaserver -y
 sudo apt-get update && sudo apt-get upgrade -y
 
 # Create autostart script
